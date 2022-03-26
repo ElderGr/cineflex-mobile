b26a278cd9584593e49d86bbf9eb3b34c0edae89
ElderGr <gbielrs@gmail.com>
2022-03-26 12:57:13 -0300

    feat: added styling in application header

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -9,14 +9,43 @@ import SuccessScreen from '../pages/Success';
 
 const Stack = createNativeStackNavigator();
 
+const defaultHeaderConfig = {
+  title: `Cineflex`,
+  headerTitleAlign: `center`,
+  headerTintColor: `#E8833A`,
+  headerTitleStyle:{
+    fontSize: 30
+  },
+  headerStyle: {
+    backgroundColor: `#C3CFD9`,
+  },
+  
+}
+
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Movie" component={MovieScreen} />
-        <Stack.Screen name="Session" component={SessionScreen} />
-        <Stack.Screen name="Success" component={SuccessScreen} />
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}
+          options={defaultHeaderConfig}
+        />
+        <Stack.Screen 
+          name="Movie" 
+          component={MovieScreen} 
+          options={defaultHeaderConfig}
+        />
+        <Stack.Screen 
+          name="Session" 
+          component={SessionScreen} 
+          options={defaultHeaderConfig}
+        />
+        <Stack.Screen 
+          name="Success" 
+          component={SuccessScreen} 
+          options={defaultHeaderConfig}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
